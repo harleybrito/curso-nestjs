95300f81beec0e927e0612a8b69781593d7ac2c3
harley <harleybrito@live.com>
2026-02-10 15:04:50 -0300

    mais implecetacao

--- src/tegami/dto/update-tegami.dto.ts
+++ src/tegami/dto/update-tegami.dto.ts
@@ -1,5 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { PartialType } from '@nestjs/mapped-types';
+import { IsBoolean, IsOptional } from 'class-validator';
 
 import { CreateTegamiDto } from './create-tegami.dto';
 
-export class UpdateTegamiDto extends PartialType(CreateTegamiDto) {}
+export class UpdateTegamiDto extends PartialType(CreateTegamiDto) {
+  @IsBoolean({ message: 'Readen must be a boolean.' })
+  @IsOptional()
+  readonly readen: boolean;
+}
